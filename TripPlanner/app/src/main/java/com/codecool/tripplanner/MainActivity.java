3e1bab3f0a21e517ec3fc24520e0fd6285acbc32
Bogi Kiss <bogikiss85@gmail.com>
2020-06-04 19:26:36 +0200

    add new city view and function. yet to correct arguments for new trip

--- TripPlanner/app/src/main/java/com/codecool/tripplanner/MainActivity.java
+++ TripPlanner/app/src/main/java/com/codecool/tripplanner/MainActivity.java
@@ -7,15 +7,22 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private TripViewModel tripViewModel;
+
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+
 
 
     @Override
@@ -30,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
 
         tripViewModel = new ViewModelProvider(this).get(TripViewModel.class);
 
-
         tripViewModel.getAllTrips().observe(this, new Observer<List<Trip>>() {
             @Override
             public void onChanged(@Nullable final List<Trip> trips) {
@@ -38,6 +44,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewTripActivity.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
     }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Trip trip = new Trip(Objects.requireNonNull(data.getStringExtra(NewTripActivity.EXTRA_CITY)),"","");
+            tripViewModel.insert(trip);
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 
 }
---
+++ TripPlanner/app/src/main/java/com/codecool/tripplanner/NewTripActivity.java
@@ -0,0 +1,55 @@
+package com.codecool.tripplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NewTripActivity extends AppCompatActivity {
+
+
+    public static final String EXTRA_CITY = "com.example.android.wordlistsql.REPLY";
+    public static final String EXTRA_CONTINENT = "com.example.android.wordlistsql.REPLY";
+    public static final String EXTRA_COUNTRY = "com.example.android.wordlistsql.REPLY";
+
+    private EditText addCity;
+    private EditText addCountry;
+    private EditText addContinent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_word);
+        addCity = findViewById(R.id.add_city);
+        addContinent = findViewById(R.id.add_continent);
+        addCountry = findViewById(R.id.add_country);
+
+
+        final Button button = findViewById(R.id.button_save);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent replyIntent = new Intent();
+                if (TextUtils.isEmpty(addCity.getText())) {
+                    setResult(RESULT_CANCELED, replyIntent);
+                } else {
+                    String city = addCity.getText().toString();
+                    String continent = addContinent.getText().toString();
+                    String country = addCountry.getText().toString();
+
+                    replyIntent.putExtra(EXTRA_CITY, city);
+
+                    replyIntent.putExtra(EXTRA_CONTINENT, continent);
+                    replyIntent.putExtra(EXTRA_COUNTRY, country);
+                    setResult(RESULT_OK, replyIntent);
+                }
+                finish();
+            }
+        });
+    }
+
+}
