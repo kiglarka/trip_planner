6b32b9584d9c134c8c446ba438f12b2aadc306e8
Bogi Kiss <bogikiss85@gmail.com>
2020-06-06 10:19:54 +0200

    implement ButterKnife in NewTripActivity

--- TripPlanner/app/src/main/java/com/codecool/tripplanner/newtrip/NewTripActivity.java
+++ TripPlanner/app/src/main/java/com/codecool/tripplanner/newtrip/NewTripActivity.java
@@ -11,21 +11,20 @@ import android.widget.Toast;
 import com.codecool.tripplanner.R;
 import com.codecool.tripplanner.Trip;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class NewTripActivity extends AppCompatActivity implements NewTripContract {
 
 
-    /*
-    public static final String EXTRA_CITY = "com.example.android.wordlistsql.REPLY";
-    public static final String EXTRA_CONTINENT = "com.example.android.wordlistsql.REPLY1";
-    public static final String EXTRA_COUNTRY = "com.example.android.wordlistsql.REPLY2";
-    public static final String EXTRA_IMAGE = "com.example.android.wordlistsql.REPLY3";
-
-     */
-
-    private EditText addCity;
-    private EditText addCountry;
-    private EditText addContinent;
-    private EditText addImage;
+    @BindView(R.id.add_city)
+    EditText addCity;
+    @BindView(R.id.add_country)
+    EditText addCountry;
+    @BindView(R.id.add_continent)
+    EditText addContinent;
+    @BindView(R.id.add_image)
+    EditText addImage;
 
     NewTripPresenter presenter;
 
@@ -33,11 +32,7 @@ public class NewTripActivity extends AppCompatActivity implements NewTripContrac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_trip);
-
-        addCity = findViewById(R.id.add_city);
-        addContinent = findViewById(R.id.add_continent);
-        addCountry = findViewById(R.id.add_country);
-        addImage = findViewById(R.id.add_image);
+        ButterKnife.bind(this);
 
         presenter = new NewTripPresenter(this);
         presenter.onAttach(this);
@@ -54,26 +49,6 @@ public class NewTripActivity extends AppCompatActivity implements NewTripContrac
 
                 Trip trip = new Trip(city, continent,country, image);
                 presenter.saveTrip(trip);
-
-
-                /*
-                Intent replyIntent = new Intent();
-                if (TextUtils.isEmpty(addCity.getText())) {
-                    setResult(RESULT_CANCELED, replyIntent);
-                } else {
-                    String city = addCity.getText().toString();
-                    String continent = addContinent.getText().toString();
-                    String country = addCountry.getText().toString();
-                    String image = addImage.getText().toString();
-
-                    replyIntent.putExtra(EXTRA_CITY, city);
-                    replyIntent.putExtra(EXTRA_CONTINENT, continent);
-                    replyIntent.putExtra(EXTRA_COUNTRY, country);
-                    replyIntent.putExtra(EXTRA_IMAGE, image);
-                    setResult(RESULT_OK, replyIntent);
-                }
-                finish();
-                */
             }
 
 
